c3b740bc390a37a7dc333b7663cdc3c824aa0e44
ssgushyonka <orangeee04@gmail.com>
2025-01-10 15:09:10 +0300

    Initial Commit

---
+++ total_billTests/total_billTests.swift
@@ -0,0 +1,17 @@
+//
+//  total_billTests.swift
+//  total_billTests
+//
+//  Created by Элина Борисова on 09.01.2025.
+//
+
+import Testing
+@testable import total_bill
+
+struct total_billTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
